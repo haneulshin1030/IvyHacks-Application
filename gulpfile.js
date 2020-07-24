63df56fd414c3e61fe69db2bc491a699b1a24f5d
Haneul Shin <haneulshin@haneuls-mbp.myfiosgateway.com>
2020-07-24 19:21:05 -0400

    update gulp 3 -> 4

--- gulpfile.js
+++ gulpfile.js
@@ -16,17 +16,17 @@ var environment = process.env.NODE_ENV;
 
 var nodemon = require('gulp-nodemon');
 
-function swallowError (error) {
-    //If you want details of the error in the console
-    console.log(error.toString());
-    this.emit('end');
+function swallowError(error) {
+  //If you want details of the error in the console
+  console.log(error.toString());
+  this.emit('end');
 }
 
-gulp.task('default', function(){
+gulp.task('default', gulp.series(async function () {
   console.log('yo. use gulp watch or something');
-});
+}));
 
-gulp.task('js', function () {
+gulp.task('js', gulp.series(async function () {
   var b = browserify({
     entries: 'app/client/src/app.js',
     debug: environment === "dev",
@@ -37,32 +37,32 @@ gulp.task('js', function () {
   b.bundle()
     .pipe(source('app.js'))
     .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(ngAnnotate())
     .on('error', swallowError)
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('app/client/build'));
-});
+}));
 
-gulp.task('sass', function() {
+gulp.task('sass', gulp.series(async function () {
   gulp.src('app/client/stylesheets/site.scss')
     .pipe(sass())
-      .on('error', sass.logError)
+    .on('error', sass.logError)
     .pipe(cleanCss())
     .pipe(gulp.dest('app/client/build'));
-});
+}));
 
-gulp.task('build', ['js', 'sass'], function() {
+gulp.task('build', gulp.series(['js', 'sass'], async function () {
   // Yup, build the js and sass.
-});
+}));
 
-gulp.task('watch', ['js', 'sass'], function() {
-  gulp.watch('app/client/src/**/*.js', ['js']);
-  gulp.watch('app/client/views/**/*.js', ['js']);
-  gulp.watch('app/client/stylesheets/**/*.scss', ['sass']);
-});
+gulp.task('watch', gulp.series(['js', 'sass'], async function () {
+  gulp.watch('app/client/src/**/*.js', gulp.series(['js']));
+  gulp.watch('app/client/views/**/*.js', gulp.series(['js']));
+  gulp.watch('app/client/stylesheets/**/*.scss', gulp.series(['js']));
+}));
 
-gulp.task('server', ['watch'], function() {
+gulp.task('server', gulp.series(['watch'], async function () {
   nodemon({
     script: 'app.js',
     env: { 'NODE_ENV': process.env.NODE_ENV || 'DEV' },
@@ -70,4 +70,4 @@ gulp.task('server', ['watch'], function() {
       'app/server'
     ]
   });
-});
+}));
